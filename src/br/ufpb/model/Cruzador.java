e080c3876ef821f479f2d159a9ce1e5ae7f1e193
Diego Sousa <diego@diegosousa.com>
2012-02-16 22:22:51 -0300

    Classe PortaAviões, Submarino, Patrulha.

--- src/br/ufpb/model/Cruzador.java
+++ src/br/ufpb/model/Cruzador.java
@@ -1,7 +1,7 @@
 package br.ufpb.model;
 
 public class Cruzador extends Destroyer {
-  public Cruzador() {
-    super(Tipo.CRUZADOR.getTam(), Tipo.CRUZADOR, Tipo.CRUZADOR.getTam());
-  }
+	public Cruzador() {
+		super(Tipo.CRUZADOR.getTam(), Tipo.CRUZADOR, Tipo.CRUZADOR.getTam());
+	}
 }
